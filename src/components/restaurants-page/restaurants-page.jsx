b8b33257ff3c6d78a8fbedd325b490eebde82a8f
Olga Konashina <o1konashina@gmail.com>
2025-10-08 19:13:58 +0300

    Add tabs

--- src/components/restaurants-page/restaurants-page.jsx
+++ src/components/restaurants-page/restaurants-page.jsx
@@ -1,12 +1,28 @@
+import { useState } from "react";
 import { restaurants } from "../../constants/mock";
 import { Restaurant } from "../restaurant/restaurant";
 
 export const RestaurantsPage = () => {
+  const [activeTabIndex, setActiveTabIndex] = useState(0);
+
   return (
     <div>
-      {restaurants.map((restaurant) => (
-        <Restaurant restaurant={restaurant} key={restaurant.id} />
+      {restaurants.map((restaurant, index) => (
+        <button
+          key={restaurant.id}
+          onClick={() => {
+            if (index !== activeTabIndex) {
+              setActiveTabIndex(index);
+            }
+          }}
+        >
+          {restaurant.name}
+        </button>
       ))}
+      <Restaurant
+        restaurant={restaurants[activeTabIndex]}
+        key={restaurants[activeTabIndex].id}
+      />
     </div>
   );
 };
